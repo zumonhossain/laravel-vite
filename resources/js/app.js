5e94b75137c769adfc32eb83bde1035b1fb482aa
zumonhossain <zumon15-13169@diu.edu.bd>
2022-09-22 12:28:43 +0600

    Project Create & Vue3 Install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue'
+// import the root component App from a single-file component.
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#app')
