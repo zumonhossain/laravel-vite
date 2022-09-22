5f07eef9a5acd66f555ce979aa2697453935981e
zumonhossain <zumon15-13169@diu.edu.bd>
2022-09-22 20:01:20 +0600

    Laravel Factory Create & API Resources

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run(){
+
+        \App\Models\Student::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
